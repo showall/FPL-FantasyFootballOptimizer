3c53198a61b26048f4adc2384b70347c1da0bf9f
showall <gerardsho@gmail.com>
2023-05-09 16:48:06 +0800

    first commit

--- app2.py
+++ app2.py
@@ -18,7 +18,8 @@ from helper import calculate_score
 from dash import dash_table
 import os
 
-app = dash.Dash(__name__, requests_pathname_prefix='/dashboard/',routes_pathname_prefix='/dashboard/', external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = dash.Dash(__name__, requests_pathname_prefix='/dashboard/',  external_stylesheets=[dbc.themes.BOOTSTRAP]) 
+                #, routes_pathname_prefix='/dashboard/
 dashboard_endpoint = "dashboard"
 
 basepic_top_y = 25
